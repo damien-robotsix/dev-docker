231d927be666f3aadf800e0492ee8d9df45a4b37
Damien SIX <damien@robotsix.net>
2024-12-29 20:05:14 +0100

    feat: Add lazy loading for which-key.nvim with updated keybindings

--- conf/nvim/lua/plugins/helpers.lua
+++ conf/nvim/lua/plugins/helpers.lua
@@ -1,11 +1,15 @@
 return {
 	{
 		"folke/which-key.nvim",
+		event = "VeryLazy",
 		keys = {
-			{ "<leader?>",
+			{
+				"??",
 				function()
-					require("which-key").show({ global = false })
-				end, }
+					require("which-key").show({ global = true })
+				end,
+				desc = "Buffer local Keymaps with which-key"
+			}
 		},
 	}
 }
