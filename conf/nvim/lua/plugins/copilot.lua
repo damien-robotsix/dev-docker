bea6b981aaf15350073a1c10fd836569f2ef9070
Damien SIX <damien@robotsix.net>
2025-01-09 22:02:41 +0100

    switch back to github/copilot plugin

--- conf/nvim/lua/plugins/copilot.lua
+++ conf/nvim/lua/plugins/copilot.lua
@@ -2,16 +2,5 @@
 vim.g.copilot_workspace_folders = { vim.fn.getcwd() }
 
 return {
-	{
-		"zbirenbaum/copilot.lua",
-		config = function()
-			require("copilot").setup({
-				suggestion = { enabled = true },
-				panel = { enabled = true },
-				filetypes = {
-					["*"] = true,
-				},
-			})
-		end
-	},
+	"github/copilot.vim"
 }
