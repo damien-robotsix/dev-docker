25236bef3b9cef1b18707ecf203f9b5db2726564
Damien SIX <damien@robotsix.net>
2025-01-11 09:55:43 +0100

    trouble plugin

---
+++ conf/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,11 @@
+return {
+	"folke/trouble.nvim",
+	opts = {
+		auto_open = true,
+		auto_close = true,
+		pinned = true
+	}, -- for default options, refer to the configuration section for custom setup.
+	cmd = "Trouble",
+	keys = {
+	},
+}
